c18c1e0803523028bede91a61a588613c06cee50
nuke-droid <33710624+nuke-droid@users.noreply.github.com>
2021-07-30 15:42:03 -0700

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 // A doubly linked list node
@@ -9,126 +10,136 @@ struct Node {
 };
 
 
-//insert a new node at the end of the list
-void insert_end(struct Node** head, int new_data)
-{
-    //allocate memory for node
-    struct Node* newNode = new Node;
+class linkedlist {
 
-    struct Node* last = *head; //set last node value to head
 
-    //set data for new node
-    newNode->data = new_data;
+public:
 
-    //new node is the last node , so set next of new node to null
-    newNode->next = NULL;
 
-    //check if list is empty, if yes make new node the head of list
-    if (*head == NULL) {
-        newNode->prev = NULL;
-        *head = newNode;
+    //insert a new node at the end of the list
+    void insert_end(struct Node** head, int new_data)
+    {
+        //allocate memory for node
+        struct Node* newNode = new Node;
+
+        struct Node* last = *head; //set last node value to head
+
+        //set data for new node
+        newNode->data = new_data;
+
+        //new node is the last node , so set next of new node to null
+        newNode->next = NULL;
+
+        //check if list is empty, if yes make new node the head of list
+        if (*head == NULL) {
+            newNode->prev = NULL;
+            *head = newNode;
+            return;
+        }
+
+        //otherwise traverse the list to go to last node
+        while (last->next != NULL)
+            last = last->next;
+
+        //set next of last to new node
+        last->next = newNode;
+
+        //set last to prev of new node
+        newNode->prev = last;
         return;
     }
 
-    //otherwise traverse the list to go to last node
-    while (last->next != NULL)
-        last = last->next;
-
-    //set next of last to new node
-    last->next = newNode;
-
-    //set last to prev of new node
-    newNode->prev = last;
-    return;
-}
-
-void del(Node** head, int position)
-{
-
-    // If linked list is empty
-    if (*head == NULL)
-        return;
-
-    // Store head node
-    Node* temp = *head;
-
-    // If head needs to be removed
-    if (position == 0)
+    void del(Node** head, int position)
     {
 
-        // Change head
-        *head = temp->next;
+        // If linked list is empty
+        if (*head == NULL)
+            return;
 
-        // Free old head
-        free(temp);
-        return;
+        // Store head node
+        Node* temp = *head;
+
+        // If head needs to be removed
+        if (position == 0)
+        {
+
+            // Change head
+            *head = temp->next;
+
+            // Free old head
+            free(temp);
+            return;
+        }
+
+        // Find previous node of the node to be deleted
+        for (int i = 0; temp != NULL && i < position - 1; i++)
+            temp = temp->next;
+
+        // If position is more than number of nodes
+        if (temp == NULL || temp->next == NULL)
+            return;
+
+        // Node temp->next is the node to be deleted
+        // Store pointer to the next of node to be deleted
+        Node* next = temp->next->next;
+
+        // Unlink the node from linked list
+        free(temp->next); // Free memory
+
+        // Unlink the deleted node from list
+        temp->next = next;
     }
 
-    // Find previous node of the node to be deleted
-    for (int i = 0; temp != NULL && i < position - 1; i++)
-        temp = temp->next;
-
-    // If position is more than number of nodes
-    if (temp == NULL || temp->next == NULL)
-        return;
-
-    // Node temp->next is the node to be deleted
-    // Store pointer to the next of node to be deleted
-    Node* next = temp->next->next;
-
-    // Unlink the node from linked list
-    free(temp->next); // Free memory
-
-    // Unlink the deleted node from list
-    temp->next = next;
-}
-
-int find(struct Node** head, int x) {
+    int find(struct Node** head, int x) {
 
 
-    //Get head node address
-    Node* temp = *head;
-    
-    //place hold for index of int in linked list
-    int pos = 0;
+        //Get head node address
+        Node* temp = *head;
 
-    //traverses doubly linked list
-    while (temp->data != x && temp->next != NULL) {
+        //place hold for index of int in linked list
+        int pos = 0;
 
-        //pos counter updates position
-        pos++;
+        //traverses doubly linked list
+        while (temp->data != x && temp->next != NULL) {
 
-        //updates temp object address
-        temp = temp->next;
+            //pos counter updates position
+            pos++;
+
+            //updates temp object address
+            temp = temp->next;
+        }
+
+        if (temp->data != x)
+            return -1;
+
+        return (pos + 1);
+
     }
 
-    if (temp->data != x)
-        return -1;
-    
-    return (pos + 1);
+    // This function prints contents of linked list starting from the given node
+    void displayList(struct Node* node) {
+        struct Node* last;
 
-}
-
-// This function prints contents of linked list starting from the given node
-void displayList(struct Node* node) {
-    struct Node* last;
-
-    while (node != NULL) {
-        cout << node->data << " | ";
-        last = node;
-        node = node->next;
+        while (node != NULL) {
+            cout << node->data << " | ";
+            last = node;
+            node = node->next;
+        }
+        if (node == NULL)
+            cout << "NULL";
     }
-    if (node == NULL)
-        cout << "NULL";
-}
 
 
 
+
+
+};
 //main program
 int main() {
     /* Start with the empty list */
     struct Node* head = NULL;
     
+    linkedlist ll;
 
     int targetDeletion, loc, choice, value;
     // Insert 40 as last node
@@ -147,16 +158,16 @@ int main() {
 
                 printf("\nEnter value for new item: \n");
                 cin >> value;
-                insert_end(&head, value);
+                ll.insert_end(&head, value);
                 cout << "List updated." << endl;
-                displayList(head);
+                ll.displayList(head);
 
                 break;
             case 2:
             
                 printf("\nEnter value for desired search: \n");
                 cin >> value;
-                loc = find(&head, value);
+                loc = ll.find(&head, value);
                 if (loc == -1) {
                     cout << "Query not found!" << endl;
 
@@ -169,15 +180,15 @@ int main() {
 
                 printf("\nEnter value for desired deletion: \n");
                 cin >> value;
-                loc = find(&head, value);
+                loc = ll.find(&head, value);
                 if (loc == -1) {
                     cout << "Query not found!" << endl;
                     exit(0);
                 }
-                del(&head, (loc - 1));
+                ll.del(&head, (loc - 1));
                 cout << "Node deleted at position " << loc << endl;
                 cout << "List updated." << endl;
-                displayList(head);
+                ll.displayList(head);
                 break;
             case 4:
                 exit(0);
