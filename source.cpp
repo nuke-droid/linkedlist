0db8163d5cdc68cac88780c1344a3b0bb121bee1
nuke-droid <jose.l.solorio.p@gmail.com>
2021-09-08 23:15:33 -0700

    added bubble sort

--- source.cpp
+++ source.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 // A doubly linked list node
@@ -129,6 +129,60 @@ public:
             cout << "NULL";
     }
 
+    /*Function to swap the nodes */
+    struct Node* swap(struct Node* ptr1, struct Node* ptr2)
+    {
+        struct Node* tmp = ptr2->next;
+        ptr2->next = ptr1;
+        ptr1->next = tmp;
+        return ptr2;
+    }
+    
+    /* Function to sort the list */
+    void bubbleSort(struct Node** head, int count)
+    {
+        struct Node** h;
+        int i, j, swapped;
+    
+        for (i = 0; i <= count; i++)
+        {
+    
+            h = head;
+            swapped = 0;
+    
+            for (j = 0; j < count - i - 1; j++) 
+            {
+    
+                struct Node* p1 = *h;
+                struct Node* p2 = p1->next;
+    
+                if (p1->data > p2->data)
+                {
+    
+                    /* update the link after swapping */
+                    *h = swap(p1, p2);
+                    swapped = 1;
+                }
+    
+                h = &(*h)->next;
+            }
+    
+            /* break if the loop ended without any swap */
+            if (swapped == 0)
+                break;
+        }
+    }
+  
+    /* Function to print the list */
+    void printList(struct Node* n)
+    {
+        while (n != NULL)
+        {
+            cout << n->data << " -> ";
+            n = n->next;
+        }
+        cout << endl;
+    }
 
 
 
@@ -140,13 +194,14 @@ int main() {
     struct Node* head = NULL;
     
     linkedlist ll;
-
-    int targetDeletion, loc, choice, value;
+    int targetDeletion, loc, choice, value, count, temp;
+    fstream file_injest;
+    string v, path;
   
 
     do {
 
-        printf("\n1. Insert Tail\n2. Find \n3. Delete \n4. Exit \nSelect Operation: ");
+        printf("\n1. Insert Tail\n2. Find \n3. Delete \n4. Inject node list \n5. Bubble sort nodes \n6. Display nodes \n7. Exit \nSelect Operation: ");
         cin >> choice;
         
 
@@ -185,10 +240,38 @@ int main() {
                 }
                 ll.del(&head, (loc - 1));
                 cout << "Node deleted at position " << loc << endl;
+                count--;
                 cout << "List updated." << endl;
                 ll.displayList(head);
                 break;
             case 4:
+                
+                cout << "Enter file path to injest: \n";
+                cin >> path;
+              
+                file_injest.open(path,ios::in); //open a file to perform read operation using file object
+                if (file_injest.is_open()){   //checking whether the file is open
+                    string tp;
+                    while(getline(file_injest, v)){ //read data from file object and put it into string.
+                       temp = stoi(v);
+                       ll.insert_end(&head, temp);
+                       cout << v;
+                       count++;
+                    }
+                    file_injest.close(); //close the file object.
+                }
+                cout << "Nodes injected successfully.\n";
+
+                break;
+            case 5:
+                
+                ll.bubbleSort(&head, count);
+                ll.printList(head);
+                break;
+            case 6:
+                ll.printList(head);
+                break;
+            case 7:
                 exit(0);
                 break;
                 
@@ -201,3 +284,4 @@ int main() {
 
     return 0;
 }
+
